82efc13ce84426b8dda39b7389d926031cb35379
agustin-candioti <agustin.candioti@hotmail.com>
2021-04-20 01:39:37 -0300

    the command lista was added. Both commands are functional, however agregar is adding NULL values to the SQL Database.

--- imdbot.py
+++ imdbot.py
@@ -3,9 +3,12 @@ import os
 import random
 import discord
 import requests
+import mysql.connector
 from dotenv import load_dotenv
 from discord.ext import commands
 from pprintpp import pprint as pp
+from prettytable import PrettyTable
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,24 +19,47 @@ KEY = os.getenv('OMDBKEY')
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 
-@bot.command(name='agregar')
-async def agregar(ctx, year, *, multi_word):
+db = mysql.connector.connect(host="localhost", user="root", passwd="1404", database="movies")
+cursor = db.cursor()
+cursor.execute("SELECT * FROM discordmovies")
+data = cursor.fetchall()
 
+@bot.command(name='agregar')
+async def agregar(ctx, imdblink):
     url='http://www.omdbapi.com/?apiKey='+KEY
     params = {
-        's':multi_word,
-        'type':'movie',
-        'y': year
+        'i': imdblink
     }
     response = requests.get(url,params=params).json()
     pp(response)
-    if len(response['Search']) > 1:
-        response = 'A ver si sos un poquito más específico la concha de tu madre'
+    if response['Response'] == "True":
+        title = response['Title']
+        year = response['Year']
+        imdbRating = response['imdbRating']
+        director = response['Director']
+        plot = response['Plot']
+        cursor.execute("INSERT INTO discordmovies (Title,Year,IMDBRating,Director,Plot) VALUES (title,year,imdbRating,director,plot)")
+        db.commit()
+        response = title+' fue agregada a la lista mi rey'
+        print(title + " " + year + " " + plot)
     else:
-        response = multi_word+' fue agregada a la lista mi rey'
+        response = "qué mierda me mandaste ridículo?"
+        print(response['Response'])
 
     await ctx.send(response)
 
-
+@bot.command(name='lista')
+async def lista(ctx):
+    cursor.execute("SELECT * FROM discordmovies")
+    data = cursor.fetchall()
+    myTable = PrettyTable(["Title", "Year", "imdbRating", "Director", "Plot"])
+    for row in data:
+        title = row[0]
+        year = row[1]
+        imdbRating = row[2]
+        director = row[3]
+        plot = row[4]
+        myTable.add_row([title, year, imdbRating, director, plot])
+    await ctx.send(myTable)
 
 bot.run(TOKEN)
